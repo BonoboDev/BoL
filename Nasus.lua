b41a099e308087e01f31a7ff1f0719107391cd43
AMBER17 <amberpayment17@gmail.com>
2016-03-25 19:52:01 +0100

    Update Nasus.lua

--- Nasus.lua
+++ Nasus.lua
@@ -42,12 +42,13 @@ function Nasus:__init()
 	self.VP = VPrediction()
 	self.TargetSelector = TargetSelector(TARGET_LESS_CAST_PRIORITY, 650 , DAMAGE_PHYSICAL, false, true)
 
-	self.jungleTable = minionManager(MINION_JUNGLE, myHero.range + 160, myHero, MINION_SORT_MAXHEALTH_DEC)
-	self.minionTable = minionManager(MINION_ENEMY, myHero.range + 160, myHero, MINION_SORT_MAXHEALTH_DEC)
+	self.jungleTable = minionManager(MINION_JUNGLE, myHero.range + 180, myHero, MINION_SORT_MAXHEALTH_DEC)
+	self.minionTable = minionManager(MINION_ENEMY, myHero.range + 180, myHero, MINION_SORT_MAXHEALTH_DEC)
 
 
 	self:Menu()
 	AddTickCallback(function() self:OnTick() end)
+	AddDrawCallback(function() self:OnDraw() end)
 end
 
 function Nasus:Menu()
@@ -71,9 +72,67 @@ function Nasus:Menu()
 		self.Settings:Menu("lasthit", "Last Hit Settings")
 			self.Settings.lasthit:KeyBinding("active", "Last Hit Key", GetKey("X"))
 			self.Settings.lasthit:Boolean("useQ", "Use (Q)", true)
+		self.Settings:Menu("drawing", "Draw Settings")
+			self.Settings.drawing:Boolean("Minion", "Draw Circle on Minion", true)
+			self.Settings.drawing:Boolean("Target", "Draw Circle on Target", true)
+			self.Settings.drawing:Boolean("Damage", "Draw (Q) Damage on Target", true)
+end
+
+function Nasus:OnDraw()
+	if myHero.dead then return end
+	if self.Settings.drawing.Minion then
+		local lowestMinion = self:findLowestMinion(self.minionTable.objects)
+		for uid, minion in pairs(self.minionTable.objects) do
+			if lowestMinion ~= nil and lowestMinion.networkID ~= minion.networkID then 
+				self:DrawCircle2(minion.x, minion.y, minion.z, 60, ARGB(180,255,0,0))
+			elseif lowestMinion == nil then
+				self:DrawCircle2(minion.x, minion.y, minion.z, 60, ARGB(180,255,0,0)) 
+			end
+		end
+		if lowestMinion ~= nil then
+			if lowestMinion.health <= self:qDmg(lowestMinion) then 
+				self:DrawCircle2(lowestMinion.x, lowestMinion.y, lowestMinion.z, 60, ARGB(180,0,255,0))
+			else
+				self:DrawCircle2(lowestMinion.x, lowestMinion.y, lowestMinion.z, 60, ARGB(180,255,153,0))
+			end 
+		end
+	end
+
+	local unit = self.TargetSelector.target
+	if self.Settings.drawing.Target and ValidTarget(unit) then
+		self:DrawCircle2(self.TargetSelector.target.x, self.TargetSelector.target.y, self.TargetSelector.target.z, 100, ARGB(180,30,255,30))
+		self:DrawCircle2(self.TargetSelector.target.x, self.TargetSelector.target.y, self.TargetSelector.target.z, 80, ARGB(180,30,255,30)) 
+	end
+	if self.Settings.drawing.Damage and ValidTarget(unit) then
+		local Center = GetUnitHPBarPos(unit)
+		if Center.x > -100 and Center.x < WINDOW_W+100 and Center.y > -100 and Center.y < WINDOW_H+100 then
+		local off = GetUnitHPBarOffset(unit)
+		local y=Center.y + (off.y * 53) + 2
+		local xOff = ({['AniviaEgg'] = -0.1,['Darius'] = -0.05,['Renekton'] = -0.05,['Sion'] = -0.05,['Thresh'] = -0.03,})[unit.charName]
+		local x = Center.x + ((xOff or 0) * 140) - 66
+		dmg = unit.health - self:qDmg(unit)
+		DrawLine(x + ((unit.health /unit.maxHealth) * 104),y, x+(((dmg > 0 and dmg or 0) / unit.maxHealth) * 104),y,9, GetDistance(unit) < 3000 and ARGB(180,205,173,0))
+		end
+	end
+end
+
+function Nasus:findLowestMinion(table)
+	local hp = 50000
+	local isMinion = nil
+	for uid, minion in pairs(table) do
+		if minion.health < hp then
+			hp = minion.health
+			isMinion = minion
+		end
+	end
+	return isMinion
 end
 
 function Nasus:OnTick()
+	self.TargetSelector:update()
+	self.minionTable:update()
+	self.jungleTable:update()
+
 	self:SetqStack()
 	self:LaneClear(self.Settings.laneclear.active)
 	self:JungleClear(self.Settings.jungleclear.active)
@@ -94,7 +153,7 @@ end
 
 function Nasus:Combo(isActive)
 	if not isActive then return end
-	self.TargetSelector:update()
+	
 	local unit  = self.TargetSelector.target
 	if ValidTarget(unit) then
 		if self.Settings.combo.useQ then self:CastQ(unit) end
@@ -131,7 +190,7 @@ end
 
 function Nasus:JungleClear(isActive)
 	if not isActive then return end
-	self.jungleTable:update()
+
 	local minion = self.jungleTable.objects[1]
 	self:qMinion(self.Settings.jungleclear.useQ, self.jungleTable.objects)
 	if minion and myHero:CanUseSpell(2) == 0 and self.Settings.jungleclear.useE and myHero.mana*100/myHero.maxMana > self.Settings.jungleclear.manaE then CastSpell(_E, minion.x, minion.z) end
@@ -139,7 +198,7 @@ end
 
 function Nasus:LaneClear(isActive)
 	if not isActive then return end
-	self.minionTable:update()
+
 	local minion = self.minionTable.objects[1]
 	self:qMinion(self.Settings.laneclear.useQ, self.minionTable.objects)
 	if minion and myHero:CanUseSpell(2) == 0 and self.Settings.laneclear.useE and myHero.mana*100/myHero.maxMana > self.Settings.laneclear.manaE then CastSpell(_E, minion.x, minion.z) end
@@ -148,9 +207,10 @@ end
 function Nasus:qMinion(isActive,table)
 	if myHero:CanUseSpell(0) ~= 0 or not isActive then return end
 	for uid, minion in pairs(table) do
-		if minion and self:qDmg(minion) < minion.health then return end
-		CastSpell(_Q)
-		myHero:Attack(minion)
+		if minion and self:qDmg(minion) > minion.health then
+			CastSpell(_Q)
+			myHero:Attack(minion)
+		end
 	end
 end
 
